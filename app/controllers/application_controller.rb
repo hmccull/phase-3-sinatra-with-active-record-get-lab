d4711ef8703294616fcfd95a4b687112511b5065
Hannah McCullough <hmac091@gmail.com>
2022-01-24 10:00:41 -0700

    commit tree ea1216f62835fe61989d8f7cef4ebd0eea02bb20

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < Sinatra::Base
 
+  # set Content-Type header for all responses
+  set :default_content_type, 'application/json'
+
   # add routes
+
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    # look up the game in the database using its ID
+    bakery = Bakery.find(params[:id])
+    # send a JSON-formatted response of the game data
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    # see the BakedGood class for the method definitions of .by_price
+    goods = BakedGood.by_price
+    goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    goods = BakedGood.most_expensive
+    goods.to_json
+  end
 
 end
